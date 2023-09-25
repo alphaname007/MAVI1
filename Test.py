2fdddc7fedeec08281c88e20bafb154a8caf0398
alpha name <danielweissenberger2016@gmail.com>
2023-09-25 12:06:37 +0200

    cleaned up files

--- Test.py
+++
@@ -1,8 +0,0 @@
-from mavi1 import MAVI1
-
-import time
-import cv2
-
-mavi:MAVI1 = MAVI1(led_count=88, led_brightness=1, field_of_view_angle=82)
-
-print(mavi.calculate_led_address_sphere((270, -90)))
